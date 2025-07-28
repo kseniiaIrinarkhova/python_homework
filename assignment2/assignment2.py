45c4b6f1196d9b1e787a5b4162dd0caf48bd6071
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-07-28 14:00:12 -0700

    Task 2: changed path to employees.csv file

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -4,6 +4,8 @@ import traceback
 
 #Task 2: Read a CSV File
 
+global employees
+
 #function that read csv file and returned employees data
 def read_employees():
     #declare variables
@@ -12,14 +14,19 @@ def read_employees():
 
     #try to read csv file
     try:
-        with open('employee.csv', 'r') as file:
+        #open file for reading
+        with open('../csv/employees.csv', 'r') as file:
+            # create reader object
             reader = csv.reader(file)
+            #get 1st line and save it to dictionary as headers
             csv_data_dict['fields'] = next(reader)
+            #loop through rows and add them to list
             for row in reader:
                 csv_rows.append(row)
+            #add list to dictionary
             csv_data_dict['rows'] = csv_rows
-            return csv_data_dict
-                
+            #return data
+            return csv_data_dict            
     except Exception as e:
         #print type of exception
         print(f"An exception occurred {type(e).__name__}")
@@ -35,8 +42,6 @@ def read_employees():
             stack_trace.append(f'File: {trace[0]}, Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
         #print stack of commands from exception
         print(f"Stack trace: {stack_trace}")
-    else:
-        print(f'Successfully read csv file!')
 
 employees = read_employees()
-print(f"{employees}")
+print(f"{employees} len={len(employees)}")
