bc807f774d7d0dcdb3094e5ef0adf17dde9c6db3
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-07-28 13:14:41 -0700

    Task 2: wrapped reading csv file to function that returned dictionary

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -3,21 +3,26 @@ import csv
 
 #Task 2: Read a CSV File
 
-#declare variables
-csv_data_dict = dict()
-csv_rows = list()
+#function that read csv file and returned employees data
+def read_employees():
+    #declare variables
+    csv_data_dict = dict()
+    csv_rows = list()
 
-#try to read csv file
-try:
-    with open('employees.csv', 'r') as file:
-        reader = csv.reader(file)
-        csv_data_dict['fields'] = next(reader)
-        for row in reader:
-            csv_rows.append(row)
-        csv_data_dict['rows'] = csv_rows
-        print(csv_data_dict)
-            
-except Exception as e:
-    print(f'Exception: {e}')
-else:
-    print(f'Successfully read csv file!')
+    #try to read csv file
+    try:
+        with open('employees.csv', 'r') as file:
+            reader = csv.reader(file)
+            csv_data_dict['fields'] = next(reader)
+            for row in reader:
+                csv_rows.append(row)
+            csv_data_dict['rows'] = csv_rows
+            return csv_data_dict
+                
+    except Exception as e:
+        print(f'Exception: {e}')
+    else:
+        print(f'Successfully read csv file!')
+
+employees = read_employees()
+print(f"{employees}")
