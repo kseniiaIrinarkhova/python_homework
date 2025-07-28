aee615ad9085fdc9ffd250edd7aa2bf093a618f0
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-07-28 14:16:04 -0700

    Task 4: added function first_name

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -66,3 +66,33 @@ def column_index(field_name):
 employee_id_column = column_index("employee_id")
 print(f'{employee_id_column}')
 
+#Task 4: Find the Employee First Name
+
+#function to get first name from specific row
+#row_number:number - number of the row to fetch data
+#return: string - first name
+def first_name(row_number):
+    #find the index of "first name" column
+    first_name_index = column_index("first_name")
+    try:
+        return employees["rows"][row_number][first_name_index]
+    except Exception as e:
+        #print type of exception
+        print(f"An exception occurred {type(e).__name__}")
+        # get and print exception message
+        message = str(e)
+        if message:
+            print(f'Exception message: {message}')
+        #trace back exception
+        trace_back = traceback.extract_tb(e.__traceback__)
+        stack_trace = list()
+        #create a stack of commands
+        for trace in trace_back:
+            stack_trace.append(f'File: {trace[0]}, Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+        #print stack of commands from exception
+        print(f"Stack trace: {stack_trace}")
+
+#check output
+print(f'{first_name(1)}')
+print(f'{first_name("2")}')
+print(f'{first_name("third")}')
