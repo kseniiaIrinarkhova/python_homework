dfaac12959a1fa1302810cd1c3f6f76223a075fe
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2025-08-05 11:59:52 -0700

    Task 1: Manipulating DataFrames

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -14,5 +14,31 @@ data = {
 task1_data_frame = pd.DataFrame(data)
 
 #check data: 
-print(task1_data_frame)
+# print(task1_data_frame)
+
+#make a copy of data
+task1_with_salary = task1_data_frame.copy()
+
+#add new column
+task1_with_salary['Salary'] = [70000, 80000, 90000]
+
+#check data: 
+# print(task1_with_salary)
+
+#make a copy of tasl DataFrame
+task1_older= task1_with_salary.copy()
+
+#Create a Series from 'Age' column and increase value by 1
+age_series = task1_older['Age'].copy() +1
+#change the column in dataFrame using new series
+task1_older['Age'] = age_series
+
+#check data: 
+# print("DataFrame with increased age")
+# print(task1_older)
+
+#save to CSV
+
+# task1_older.to_csv('employees.csv', sep=',', index=False)
+
 
